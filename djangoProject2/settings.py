e1ce42a347977837db601e4ff8403a2aa826d21e
nastyaizofatova <izofatova.anastasia@yandex.ru>
2023-09-18 14:57:49 +0300

    commit 6

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -192,3 +192,5 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+SWAGGER_SETTINGS = { "DEFAULT_GENERATOR_CLASS": "rest_framework.schemas.generators.BaseSchemaGenerator", }
